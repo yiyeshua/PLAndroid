8ece608a1c6c7da9be090b6265a17d198b21712e
yiyeshua <784148786@qq.com>
2017-05-17 16:24:40 +0800

    修改部分工具类

--- app/src/main/java/com/yiyeshu/common/utils/ImageUtils.java
+++ app/src/main/java/com/yiyeshu/common/utils/ImageUtils.java
@@ -15,7 +15,6 @@ import android.graphics.Canvas;
 import android.graphics.LinearGradient;
 import android.graphics.Matrix;
 import android.graphics.Paint;
-import android.graphics.PixelFormat;
 import android.graphics.PorterDuff.Mode;
 import android.graphics.PorterDuffXfermode;
 import android.graphics.Rect;
@@ -29,6 +28,7 @@ import android.os.Environment;
 import android.provider.DocumentsContract;
 import android.provider.MediaStore;
 import android.provider.MediaStore.MediaColumns;
+import android.text.TextUtils;
 import android.util.DisplayMetrics;
 
 import java.io.BufferedOutputStream;
@@ -494,24 +494,6 @@ public class ImageUtils {
         return resizedBitmap;
     }
 
-    /**
-     * 将Drawable转化为Bitmap
-     *
-     * @param drawable
-     * @return
-     */
-    public static Bitmap drawableToBitmap(Drawable drawable) {
-        int width = drawable.getIntrinsicWidth();
-        int height = drawable.getIntrinsicHeight();
-        Bitmap bitmap = Bitmap.createBitmap(width, height, drawable
-                .getOpacity() != PixelFormat.OPAQUE ? Config.ARGB_8888
-                : Config.RGB_565);
-        Canvas canvas = new Canvas(bitmap);
-        drawable.setBounds(0, 0, width, height);
-        drawable.draw(canvas);
-        return bitmap;
-
-    }
 
     /**
      * 获得圆角图片的方法
@@ -583,16 +565,7 @@ public class ImageUtils {
         return bitmapWithReflection;
     }
 
-    /**
-     * 将bitmap转化为drawable
-     *
-     * @param bitmap
-     * @return
-     */
-    public static Drawable bitmapToDrawable(Bitmap bitmap) {
-        Drawable drawable = new BitmapDrawable(bitmap);
-        return drawable;
-    }
+
 
     /**
      * 获取图片类型
@@ -803,5 +776,177 @@ public class ImageUtils {
             return bitmap;
         } else
             return null;
+    }
+
+    /**
+     * 转化bitmap为字节数组
+     *
+     * @param b
+     * @return
+     */
+    public static byte[] bitmapToByte(Bitmap b) {
+        if (b == null) {
+            return null;
+        }
+
+        ByteArrayOutputStream o = new ByteArrayOutputStream();
+        b.compress(Bitmap.CompressFormat.PNG, 100, o);
+        return o.toByteArray();
+    }
+
+    /**
+     * 转化字节数组为bitmap
+     *
+     * @param b
+     * @return
+     */
+    public static Bitmap byteToBitmap(byte[] b) {
+        return (b == null || b.length == 0) ? null : BitmapFactory.decodeByteArray(b, 0, b.length);
+    }
+
+    /**
+     * 转化图片资源为bitmap
+     *
+     * @param d
+     * @return
+     */
+    public static Bitmap drawableToBitmap(Drawable d) {
+        return d == null ? null : ((BitmapDrawable)d).getBitmap();
+    }
+
+    /**
+     * 转化bitmap为图片资源
+     *
+     * @param b
+     * @return
+     */
+    @SuppressWarnings("deprecation")
+    public static Drawable bitmapToDrawable(Bitmap b) {
+        return b == null ? null : new BitmapDrawable(b);
+    }
+
+    /**
+     * convert Drawable to byte array
+     *
+     * @param d
+     * @return
+     */
+    public static byte[] drawableToByte(Drawable d) {
+        return bitmapToByte(drawableToBitmap(d));
+    }
+
+    /**
+     * convert byte array to Drawable
+     *
+     * @param b
+     * @return
+     */
+    public static Drawable byteToDrawable(byte[] b) {
+        return bitmapToDrawable(byteToBitmap(b));
+    }
+
+    /**
+     * scale image
+     *
+     * @param org
+     * @param newWidth
+     * @param newHeight
+     * @return
+     */
+    public static Bitmap scaleImageTo(Bitmap org, int newWidth, int newHeight) {
+        return scaleImage(org, (float)newWidth / org.getWidth(), (float)newHeight / org.getHeight());
+    }
+
+    /**
+     * scale image
+     *
+     * @param org
+     * @param scaleWidth sacle of width
+     * @param scaleHeight scale of height
+     * @return
+     */
+    public static Bitmap scaleImage(Bitmap org, float scaleWidth, float scaleHeight) {
+        if (org == null) {
+            return null;
+        }
+
+        Matrix matrix = new Matrix();
+        matrix.postScale(scaleWidth, scaleHeight);
+        return Bitmap.createBitmap(org, 0, 0, org.getWidth(), org.getHeight(), matrix, true);
+    }
+
+    public static final class InputStreamAndLength {
+        public InputStream stream;
+        public int length;
+    }
+
+    public static boolean checkImageFileExist(File imageFile) {
+        if (imageFile == null || !imageFile.exists() || !imageFile.isFile()
+                || imageFile.length() <= 0) {
+            return false;
+        }
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        opts.inSampleSize = 4;
+        opts.inPreferredConfig = Config.ALPHA_8;
+        Bitmap b = null;
+        for (int i = 0; i < 3; i++) {
+            b = BitmapFactory.decodeFile(imageFile.getAbsolutePath(), opts);
+            if (b != null) {
+                b.recycle();
+                return true;
+            } else {
+                try {
+                    Thread.sleep(300);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        }// end for
+        return false;
+    }
+
+    public static boolean checkImageFileExist(String imagePath) {
+        if (TextUtils.isEmpty(imagePath)) {
+            return false;
+        }
+        return checkImageFileExist(new File(imagePath));
+    }
+
+    public static Params getImageParams(File imageFile) {
+        if (imageFile == null || !imageFile.exists() || !imageFile.isFile()
+                || imageFile.length() <= 0) {
+            LogUtil.v("imageFile is not exist");
+            return null;
+        }
+        BitmapFactory.Options opts = new BitmapFactory.Options();
+        Bitmap sourceBp = BitmapFactory.decodeFile(imageFile.getAbsolutePath(),
+                opts);
+        return getImageParams(sourceBp);
+    }
+
+    public static Params getImageParams(String imagePath) {
+        if (TextUtils.isEmpty(imagePath)) {
+            return null;
+        }
+        return getImageParams(new File(imagePath));
+    }
+
+    public static Params getImageParams(Bitmap bitmap) {
+        if (bitmap == null) {
+            LogUtil.v("bitmap=null");
+            return null;
+        }
+        Params params = new Params();
+        params.width = bitmap.getWidth();
+        params.height = bitmap.getHeight();
+        if (bitmap != null && !bitmap.isRecycled()) {
+            bitmap.recycle();
+            bitmap = null;
+        }
+        return params;
+    }
+    static class Params {
+        public int width;
+        public int height;
     }
 }
